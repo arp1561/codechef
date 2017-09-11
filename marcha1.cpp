660d34b876e18d5f4629e8fdc554d0886e58a8a3
arp1561 <joshiarpit2@gmail.com>
2017-09-11 22:46:28 +0530

    New files

--- marcha1.cpp
+++ marcha1.cpp
@@ -1,39 +1,29 @@
 #include<bits/stdc++.h>
+#define int long long
 using namespace std;
-int main()
+signed main()
 {
     int T; cin>>T;
     while(T--)
     {
-        int n,m,x,sum=0; cin>>n>>m;
-        list<int> a(n);
-        for(list<int>::iterator it=a.begin();it!=a.end();it++)
-        {
-            cin>>x;
-            *it=x;
-        }
-        a.sort();
-        a.reverse();
-        list<int>::iterator it = a.begin();
-        while(*it>m and a.size()!=0)
-        {
-            a.pop_front();
-            it=a.begin();
-        }
-        it = a.begin();
-        sum=*it;
-        a.pop_front();
-        for(it=a.begin();it!=a.end();++it)
-        {
-            if(sum==m)
-                break;
-            else if(sum+*it<=m)
-                sum+=*it;
-        }
-        if(sum==m)
-            cout<<"Yes\n";
-        else
-            cout<<"No\n";
+        int n; cin>>n;
+        int m; cin>>m;
+        int a[n];
+        for(int i=0;i<n;i++) cin>>a[i];
+        int sols[n*n][n];
 
+        cout<<endl;
+
+        for(int i=0;i<n*n;i++)
+        {
+            for(int j=0;j<n;j++)
+                sols[i][j]=0;
+        }
+        for(int i=0;i<n*n;i++)
+        {
+            for(int j=0;j<n;j++)
+                cout<<sols[i][j]<<" ";
+            cout<<endl;
+        }
     }
 }
--- test.cpp
+++ test.cpp
@@ -1,9 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+int presum(int a[],int n,int index)
+{
+    return accumulate(a,a+index+1,0);
+}
+int sufsum(int a[],int n,int index)
+{
+    return accumulate(a+index,a+n,0);
+}
+
 int main()
 {
-    int n=40;
-    
-    string s=to_string(n-1);
-    cout<<s;
+    int a[]={2,1,3,1};
+    int n=4;
+    int index = 3;
+    cout<<presum(a,n,index)<<" "<<sufsum(a,n,index);
+
 }
+
+
